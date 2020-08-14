526673ecf39c4c2cd173a2e5af574d3ac161bc5d
Esteban Indiveri <esteban.indiveri@gmail.com>
2020-08-14 01:57:42 -0300

    start testing

--- src/test/hooks/useCounterPage.test.js
+++ src/test/hooks/useCounterPage.test.js
@@ -1,15 +1,48 @@
 import useCounterPage from '../../hooks/useCounterPage';
-import { renderHook } from "@testing-library/react-hooks";
+import { renderHook,act } from "@testing-library/react-hooks";
 import '@testing-library/jest-dom';
 
 describe('pruebas en useCounterPage', () => {
     test('debe de retornar valores por defecto', () => {
         const {result}=renderHook(()=>useCounterPage());
-        console.log(result.current);
         expect(result.current.counter).toBe(10);
         expect(typeof result.current.decrement).toBe('function');
         expect(typeof result.current.increment).toBe('function');
         expect(typeof result.current.reset).toBe('function');
     });
+
+    test('counter en 100', () => {
+        const {result}=renderHook(()=>useCounterPage(100));
+        expect(result.current.counter).toBe(100);
+    });
+    test('incrementar el counter en 1', () => {
+        const {result}=renderHook(()=>useCounterPage(100));
+        const{increment}=result.current;
+        act(()=>{
+            increment();
+        });
+        const{ counter}=result.current;
+        expect(counter).toBe(101);
+    });
+    test('decrementar el counter en 1', () => {
+        const {result}=renderHook(()=>useCounterPage(100));
+        const{decrement}=result.current;
+        act(()=>{
+            decrement();
+        });
+        const{ counter}=result.current;
+        expect(counter).toBe(99);
+    });
+    test('reset del counter a 100', () => {
+        const {result}=renderHook(()=>useCounterPage(100));
+        const{increment,reset}=result.current;
+        act(()=>{
+            increment();
+            reset();
+        });
+        const{ counter}=result.current;
+        expect(counter).toBe(100);
+    });
+    
     
 })
