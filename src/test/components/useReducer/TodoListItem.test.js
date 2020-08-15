8b1610d887a90e4082bb0e6a47c1247072a7c486
Esteban Indiveri <esteban.indiveri@gmail.com>
2020-08-15 18:53:44 -0300

    upload test

--- src/test/components/useReducer/TodoListItem.test.js
+++ src/test/components/useReducer/TodoListItem.test.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import TodoListItem from '../../../components/useReducer/TodoListItem';
-
+import { demoTodos } from '../../fixtures/demoTodos';
+import '@testing-library/jest-dom';
 
 describe('<TodoListItem/>', () => {
-    const wrapper=shallow(<TodoListItem todo/>)
+    const handleDelete=jest.fn();
+    const handleToggle=jest.fn();
+    const wrapper=shallow(<TodoListItem 
+        todo={demoTodos[0]} 
+        index={demoTodos[0].id} 
+        handleDelete={handleDelete} 
+        handleToggle={handleToggle}
+        />)
     test('debe de mostrar correctamente', () => {
         //snap
+        expect(wrapper).toMatchSnapshot();
     });
 
     test('llama function handleDelete', () => {
